326f94222c3544e9edc38e834931a15c72a32582
PavelLogeiko <114857717+PavelLogeiko@users.noreply.github.com>
2023-09-08 22:58:50 +0300

    Create Employee.java

---
+++ Lesson3/src/Employee.java
@@ -0,0 +1,96 @@
+package ru.geekbrains.lesson3;
+
+import java.util.Random;
+
+public abstract class Employee implements Comparable<Employee> {
+
+    /**
+     * Расчет среднемесячной заработной платы
+     * @return
+     */
+    public abstract double calculateSalary();
+
+    @Override
+    public int compareTo(Employee o) {
+        return Double.compare(calculateSalary(), o.calculateSalary());
+        //return calculateSalary() == o.calculateSalary() ? 0 :
+        //        calculateSalary() > o.calculateSalary() ? 1 : -1;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    {
+
+        id = ++counter;
+    }
+
+    private Employee(){
+        this("#Surnane#", "#Name#");
+    }
+
+    private Employee(String surName, String name){
+        this(surName, name, 500);
+    }
+
+    protected Employee(String surName, String name, double salary){
+        if (salary < 500){
+            throw new RuntimeException("Ставка заработной платы должна быть не менее 500");
+        }
+        this.surName = surName;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 30000){
+            throw new RuntimeException("Уровень заработной платы должен быть не менее 30000");
+        }
+        this.salary = salary;
+    }
+
+    private int id;
+
+    /**
+     * Имя
+     */
+    protected String name;
+
+    /**
+     * Фамилия
+     */
+    protected String surName;
+
+    /**
+     * Ставка заработной платы
+     */
+    protected double salary;
+
+    protected static String[] names = new String[] { "Анатолий", "Глеб", "Клим", "Мартин", "Лазарь", "Владлен", "Клим", "Панкратий", "Рубен", "Герман" };
+    protected static String[] surNames = new String[] { "Григорьев", "Фокин", "Шестаков", "Хохлов", "Шубин", "Бирюков", "Копылов", "Горбунов", "Лыткин", "Соколов" };
+    protected static Random random = new Random();
+    private static int counter = 1000;
+}
